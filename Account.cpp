b1c999035b58d4523c27cb2c1658accd7f7648a1
userAsadbek <250123@newuu.uz>
2025-11-04 23:59:02 +0500

    Implement withdraw method in Account class

---
+++ Account.cpp
@@ -0,0 +1,8 @@
+#include "Account.h"
+bool Account::withdraw(double amount){
+    if (balance<amount) return false; 
+    else { 
+        balance-=amount;
+        transactions++;
+        return true;
+    }}
